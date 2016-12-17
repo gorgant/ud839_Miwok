0dd9dbe09faa8fe06cecb321221958354263919b
Greg Ishktar <Greg Ishktar>
2016-12-17 11:37:01 -0500

    Add onClickListener code for numbers

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -32,10 +32,23 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        // Set a click listener on that View
+        numbers.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        
     }
 
-    public void openNumbersList(View view){
-        Intent intent = new Intent(this, NumbersActivity.class);
-        startActivity(intent);
-    }
+
+
 }
