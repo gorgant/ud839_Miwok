4eb5134d7181d82816f43d0b5737ce6f718625f8
Greg Ishktar <Greg Ishktar>
2016-12-19 20:08:26 -0500

    Implement several TextView iterations, including using a for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,12 +17,51 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static android.R.id.list;
+import static com.example.android.miwok.R.id.numbers;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    String TAG = "NumbersActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create list of numbers
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for(int i = 0; i < words.size(); i++ ) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+
+        for(int i = 0; i < words.size(); i++ ) {
+            Log.v(TAG,"Index: " + i + " Value: " + words.get(i));
+        }
+
+
+
+
     }
 }
