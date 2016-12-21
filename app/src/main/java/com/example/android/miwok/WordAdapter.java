a889bc405e930de4ad7a5dac4c63f8d8fa2f0aaf
Greg Ishktar <Greg Ishktar>
2016-12-21 07:04:48 -0500

    Add custom array adapter for Word object

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,58 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ludeyu on 12/20/2016.
+ */
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the list is the data we want
+     * to populate into the lists.
+     *
+     * @param context The current context. Used to inflate the layout file.
+     * @param words A List of Word objects to display in a list
+     */
+
+    public WordAdapter(Context context, ArrayList<Word> words) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        // Get the data object for this position
+        Word currentWord = getItem(position);
+        // Lookup view for data population
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Populate the data into the template view using the data object
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+        // Return the completed view to render on screen
+        return listItemView;
+
+
+    }
+}
