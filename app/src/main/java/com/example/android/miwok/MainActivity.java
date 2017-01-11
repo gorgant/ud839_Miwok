64576155893903e8fb6709284568819eae449056
Greg Ishktar <Greg Ishktar>
2017-01-10 19:11:52 -0500

    testing a commit on the origin branch (vs audio branch)

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,14 +15,14 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.TextView;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.TextView;
 
-import static android.os.Build.VERSION_CODES.M;
+        import static android.os.Build.VERSION_CODES.M;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -81,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    //test
 
 
 
