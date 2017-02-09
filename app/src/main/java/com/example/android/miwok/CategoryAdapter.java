7c40b3714c6027a62c96f7a9dfd58b05a4d05d65
Greg Ishktar <Greg Ishktar>
2017-02-08 19:25:03 -0500

    Link fragments to tabs and add visual polish

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,72 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+
+    /**
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
+     */
+    public CategoryAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        switch (position) {
+            case 0:
+                return context.getString(R.string.category_numbers);
+            case 1:
+                return context.getString(R.string.category_family);
+            case 2:
+                return context.getString(R.string.category_colors);
+            case 3:
+                return context.getString(R.string.category_phrases);
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Return the total number of pages.
+     */
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
